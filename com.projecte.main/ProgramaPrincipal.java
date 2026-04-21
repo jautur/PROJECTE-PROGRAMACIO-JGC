29cc13a9a9ead1806a834a68b179fb7ba020a4b6
Jaume Tur <jautursis@alu.edu.gva.es>
2026-04-21 18:42:53 +0200

    Projecte inicial JGC

---
+++ com.projecte.main/ProgramaPrincipal.java
@@ -0,0 +1,22 @@
+package com.projecte.main;
+
+import com.projecte.jaume.AlumneJaume;
+import com.projecte.georgi.AlumneGeorgi;
+import com.projecte.carles.AlumneCarles;
+import com.projecte.gestor.GestorDades;
+
+public class ProgramaPrincipal {
+
+    public static void main(String[] args) {
+        inici();
+    }
+
+    public static void inici() {
+        System.out.println("=== PROJECTE PROGRAMACIO JGC ===");
+        System.out.println("========== INTEGRANTS ==========");
+
+        AlumneJaume.mostrarNom();
+        AlumneGeorgi.mostrarNom();
+        AlumneCarles.mostrarNom();
+    }
+}
